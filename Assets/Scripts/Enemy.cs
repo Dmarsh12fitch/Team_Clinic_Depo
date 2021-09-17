a8190013b85fe4b4bc7de4f7bcb10410672ef5d2
David Marshall <dmarsh12@student.fitchburgstate.edu>
2021-09-17 09:45:14 -0400

    testing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void enemyHit(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            deathSequence();
+        }
+    }
+
+
+    void deathSequence()
+    {
+        //disable this enemy's functionality
+        //do death anim
+        Destroy(gameObject, 1);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage;
+    public float range;
+
+    public Camera playerCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+
+    }
+
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            hit.transform.GetComponent<Enemy>();
+
+            Enemy e = hit.transform.GetComponent<Enemy>();
+            if(e != null)
+            {
+                e.enemyHit(damage);
+            }
+        }
+    }
+
+}
